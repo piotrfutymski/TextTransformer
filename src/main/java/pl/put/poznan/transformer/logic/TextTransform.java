94d6dfcc3b82ef62ebe11bb84ab2dc59aca87eae
brusju <jakubkasprzak6899@gmail.com>
2021-01-04 17:07:46 +0100

    add comment to TextTransform

--- src/main/java/pl/put/poznan/transformer/logic/TextTransform.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransform.java
@@ -1,5 +1,13 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Interface that represents text transformation
+ */
 public interface TextTransform {
+    /**
+     * Function that perform the transformation
+     * @param text input string that will be transformed
+     * @return the result of transformation
+     */
     public abstract String transform(String text);
 }
