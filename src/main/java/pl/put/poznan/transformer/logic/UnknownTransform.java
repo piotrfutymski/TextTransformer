7f43ed191773a8211306a55c22d2224947543d5c
brusju <jakubkasprzak6899@gmail.com>
2021-01-04 18:49:09 +0100

    add comments to UnknownTransform

--- src/main/java/pl/put/poznan/transformer/logic/UnknownTransform.java
+++ src/main/java/pl/put/poznan/transformer/logic/UnknownTransform.java
@@ -1,12 +1,23 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Exception thrown when unknown transformation name is encountered
+ */
 public class UnknownTransform extends Exception {
     private String transformName;
 
+    /**
+     * Constructor
+     * @param name unknown transformation name
+     */
     public UnknownTransform(String name) {
         transformName=name;
     }
 
+    /**
+     * Get the encountered unknown transformation name
+     * @return unknown transformation name
+     */
     public String getTransformName() {
         return transformName;
     }
