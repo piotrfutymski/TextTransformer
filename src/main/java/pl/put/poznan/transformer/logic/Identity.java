ac8c6128dde6a85b7ee951deb2a40a7f0c999b69
brusju <jakubkasprzak6899@gmail.com>
2021-01-04 17:04:53 +0100

    add comments to Identity

--- src/main/java/pl/put/poznan/transformer/logic/Identity.java
+++ src/main/java/pl/put/poznan/transformer/logic/Identity.java
@@ -1,7 +1,14 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Identity is an implementation of TextTransform Interface. It represents transformation that does nothing
+ */
 public class Identity implements TextTransform{
-
+    /**
+     * Returns unchanged text
+     * @param text is input string
+     * @return unchanged input string
+     */
     @Override
     public String transform(String text) {
         return text;
