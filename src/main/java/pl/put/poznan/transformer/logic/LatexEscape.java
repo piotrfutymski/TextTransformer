2046a35ac9d53f2edffa8fa995390e64603bdf3c
brusju <jakubkasprzak6899@gmail.com>
2021-01-27 15:14:10 +0100

    add LatexEscape transform

---
+++ src/main/java/pl/put/poznan/transformer/logic/LatexEscape.java
@@ -0,0 +1,67 @@
+package pl.put.poznan.transformer.logic;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Arrays.asList;
+
+public class LatexEscape extends TextDecorator {
+
+    private Map<String,String> latexCharMap;
+    /**
+     * Constructor of LatexEscape
+     * @param t transformation that will be performed after the operation
+     */
+    LatexEscape(TextTransform t) {
+        super(t);
+    }
+
+    /**
+     * Function that escapes Latex special characters
+     * @param text input string
+     * @return text with Latex special characters escaped
+     */
+    @Override
+    protected String operation(String text) {
+        String[] res = { text };
+        if(latexCharMap == null)
+            loadLatexEscapeInfo();
+        latexCharMap.forEach((from, to) ->{
+            Matcher matcher = Pattern.compile(Pattern.quote(from)).matcher(res[0]);
+            res[0] = matcher.replaceAll(matcher.quoteReplacement(to));
+        });
+        return res[0];
+    }
+
+    private void loadLatexEscapeInfo()
+    {
+        JSONParser jsonParser = new JSONParser();
+        latexCharMap = new HashMap<String,String>();
+        try{
+            InputStream input = getClass().getResourceAsStream("/LatexEscapeInfo.json");
+            Reader reader = new InputStreamReader(input);
+            JSONArray charMap = (JSONArray)jsonParser.parse(reader);
+            for(Object it: charMap){
+                JSONObject jContainer = (JSONObject) it;//(String) jContainer.get("from");
+                latexCharMap.put((String)jContainer.get("from"), (String) jContainer.get("to"));
+            }
+            reader.close();
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+    }
+}
